6a06f0e4af9f2b224a799278d0ba4b81ab3344aa
hanahanoo <sofiatunnisahana@gmail.com>
2026-09-03 11:25:27 +0700

    Halaman Login

--- frontend-fixIT/src/router/index.js
+++ frontend-fixIT/src/router/index.js
@@ -3,10 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 // ─── Import semua halaman (views) ─────────────────────────
 // Halaman Publik (bisa diakses tanpa login)
 import HomeView     from '../public/HomeView.vue'
+import LoginView     from '../Auth/LoginView.vue'
 
 const routes = [
   // Halaman publik (bisa diakses tanpa login)
   { path: '/home',           component: HomeView,   name: 'home' },
+  { path: '/login',           component: LoginView,   name: 'login' },
 
 ]
 
